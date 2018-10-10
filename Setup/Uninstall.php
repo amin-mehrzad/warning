56ea7677d1a7e02c4a5eea5f01be8f807c474254
amin-mehrzad <43980359+amin-mehrzad@users.noreply.github.com>
2018-10-10 12:09:31 -0700

    Update Uninstall.php

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -1,23 +1,23 @@
 <?php
-namespace XCode\NicotineWremove\Setup {
+namespace XCode\NicotineWarning\Setup;
 
-    class Uninstall implements \Magento\Framework\Setup\UninstallInterface
-    {    
-        protected $eavSetupFactory;
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
 
-        public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
-        {
-            $this->eavSetupFactory = $eavSetupFactory;
-        }
 
-        public function uninstall(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
-        {
-              
-            $eavSetup = $this->eavSetupFactory->create();    
-            $entityTypeId = 4; // value for eav_entity_type table, here catalog_product value is 4
-            $eavSetup->removeAttribute($entityTypeId, 'nicotine_warning');    
-          
-        }
+class Uninstall implements UninstallInterface
+{
+    private $eavSetupFactory;
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
     }
-
+    public function uninstall(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+		$eavSetup = $this->eavSetupFactory->create();
+		$entityTypeId = 4;
+        $eavSetup->removeAttribute($entityTypeId, 'nicotine_warning');
+			
+    }
 }
