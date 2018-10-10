33dbaefa42527ca857f230babda4e77b23b93e5e
amin-mehrzad <43980359+amin-mehrzad@users.noreply.github.com>
2018-10-10 09:26:48 -0700

    Create Uninstall.php

---
+++ Setup/Uninstall.php
@@ -0,0 +1,23 @@
+<?php
+namespace XCode\NicotineWremove\Setup {
+
+    class Uninstall implements \Magento\Framework\Setup\UninstallInterface
+    {    
+        protected $eavSetupFactory;
+
+        public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+        {
+            $this->eavSetupFactory = $eavSetupFactory;
+        }
+
+        public function uninstall(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
+        {
+            $setup->startSetup();    
+            $eavSetup = $this->eavSetupFactory->create();    
+            $entityTypeId = 4; // value for eav_entity_type table, here catalog_product value is 4
+            $eavSetup->removeAttribute($entityTypeId, 'nicotine_w');    
+            $setup->endSetup();    
+        }
+    }
+
+}
