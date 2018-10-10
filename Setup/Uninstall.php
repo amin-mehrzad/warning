0482693bbace209ae1521b6a1bbf5f3118e506e3
amin-mehrzad <43980359+amin-mehrzad@users.noreply.github.com>
2018-10-10 11:19:48 -0700

    Update Uninstall.php

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -12,11 +12,11 @@ namespace XCode\NicotineWremove\Setup {
 
         public function uninstall(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
         {
-            $setup->startSetup();    
+              
             $eavSetup = $this->eavSetupFactory->create();    
             $entityTypeId = 4; // value for eav_entity_type table, here catalog_product value is 4
             $eavSetup->removeAttribute($entityTypeId, 'nicotine_warning');    
-            $setup->endSetup();    
+          
         }
     }
 
